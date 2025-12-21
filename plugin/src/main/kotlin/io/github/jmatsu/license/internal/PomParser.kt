0274398af677723d355e1fbdcc628a3f8855d1cf
Jumpei Matsuda <4340693+jmatsu@users.noreply.github.com>
2025-12-21 23:53:07 +0900

    fix: Replace groovy's XmlParser to javax xml

--- plugin/src/main/kotlin/io/github/jmatsu/license/internal/PomParser.kt
+++ plugin/src/main/kotlin/io/github/jmatsu/license/internal/PomParser.kt
@@ -1,34 +1,42 @@
 package io.github.jmatsu.license.internal
 
-import groovy.util.XmlSlurper
-import groovy.util.slurpersupport.GPathResult
 import io.github.jmatsu.license.model.LicenseSeed
 import io.github.jmatsu.license.model.ResolvedPomFile
+import org.w3c.dom.Element
+import org.w3c.dom.Node
+import org.w3c.dom.NodeList
 import java.io.File
+import javax.xml.parsers.DocumentBuilderFactory
 
 class PomParser(
     private val file: File,
 ) {
     fun parse(): ResolvedPomFile {
-        val pomRoot = XmlSlurper(false, false).parse(file)
-
-        val associatedUrl: String? = pomRoot["url"]?.trimText() ?: pomRoot["scm.url"]?.trimText()
-
-        val displayNameCandidates =
-            arrayOf(
-                pomRoot["name"],
-                pomRoot["description"],
-                pomRoot["artifactId"],
-            ).mapNotNull {
-                it?.trimText()
+        val factory =
+            DocumentBuilderFactory.newInstance().apply {
+                isValidating = false
+                isNamespaceAware = false
+            }
+        val pomRoot =
+            factory.newDocumentBuilder().parse(file).documentElement.apply {
+                normalize()
             }
 
+        val associatedUrl: String? = pomRoot.getOptionalAttribute("url") ?: pomRoot.getOptionalAttribute("scm.url")
+
+        val displayNameCandidates =
+            listOfNotNull(
+                pomRoot.getOptionalAttribute("name"),
+                pomRoot.getOptionalAttribute("description"),
+                pomRoot.getOptionalAttribute("artifactId"),
+            )
+
         val licenses: List<LicenseSeed> =
-            pomRoot["licenses"]
-                .childPaths()
+            pomRoot
+                .getChildElementsOfTag("licenses")
                 .map {
-                    val name = it["name"]?.trimText()
-                    val url = it["url"]?.trimText()
+                    val name = it.getOptionalAttribute("name")
+                    val url = it.getOptionalAttribute("url")
                     // Is distribution node required? :thinking_face:
                     LicenseSeed(
                         name = name,
@@ -37,10 +45,10 @@ class PomParser(
                 }
 
         val copyrightHolders =
-            pomRoot["developers"]
-                .childPaths()
+            pomRoot
+                .getChildElementsOfTag("developers")
                 .mapNotNull {
-                    it["name"]?.trimText()
+                    it.getOptionalAttribute("name")
                 }
 
         require(displayNameCandidates.isNotEmpty())
@@ -53,12 +61,33 @@ class PomParser(
         )
     }
 
-    private operator fun GPathResult?.get(path: String): GPathResult? =
-        path.split(".").fold(this) { acc, name ->
-            acc?.getProperty(name) as? GPathResult
+    private fun Element.getOptionalAttribute(
+        name: String,
+        allowBlank: Boolean = false,
+    ): String? =
+        if (hasAttribute(name)) {
+            getAttribute(name).takeIf { allowBlank || it.isNotBlank() }
+        } else {
+            null
         }
 
-    private fun GPathResult.trimText(): String? = this.text()?.takeIf { it.isNotBlank() }
+    private fun NodeList.asList(): List<Node> =
+        (0 until length).map { idx ->
+            item(idx)
+        }
 
-    private fun GPathResult?.childPaths(): List<GPathResult> = this?.children()?.map { it as GPathResult }.orEmpty()
+    private fun Element.getChildElementsOfTag(name: String): List<Element> {
+        val nodes = getElementsByTagName(name)
+
+        return nodes
+            .asList()
+            .flatMap { it.childNodes.asList() }
+            .mapNotNull {
+                if (it.nodeType == Node.ELEMENT_NODE) {
+                    it as Element
+                } else {
+                    null
+                }
+            }
+    }
 }
